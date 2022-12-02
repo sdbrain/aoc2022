0468a8d2b4912e00a1d14bc0ad540b68975a3d9e
David Sounthiraraj <dsounthi@cisco.com>
2022-12-01 18:07:45 -0600

    updated README

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 #### AOC 2022
 
-AOC 2022.. :)
+Code for AOC 2022..https://adventofcode.com/
 
+#### Day1b bench
+
+two approaches, vector based and binary heap.. 
+
+```
+day1b - Vector          time:   [39.821 µs 39.904 µs 39.996 µs]
+                        change: [-0.4886% -0.2241% +0.0239%] (p = 0.08 > 0.05)
+
+day1b - BinaryHeap      time:   [47.648 µs 47.754 µs 47.868 µs]
+                        change: [-0.0474% +0.2594% +0.6312%] (p = 0.15 > 0.05)
+
+```
 
--- benches/aoc.rs
+++ benches/aoc.rs
@@ -5,8 +5,10 @@ use std::fs;
 pub fn criterion_benchmark(c: &mut Criterion) {
     let input = fs::read_to_string("./src/data/day1.txt").unwrap();
     let day1 = day1::Day1::new();
-    c.bench_function("day1bv1", |b| b.iter(|| day1.top_3_elves_v1(&input)));
-    c.bench_function("day1bv2", |b| b.iter(|| day1.top_3_elves_v2(&input)));
+    c.bench_function("day1b - Vector", |b| b.iter(|| day1.top_3_elves_v1(&input)));
+    c.bench_function("day1b - BinaryHeap", |b| {
+        b.iter(|| day1.top_3_elves_v2(&input))
+    });
 }
 
 criterion_group!(benches, criterion_benchmark);
